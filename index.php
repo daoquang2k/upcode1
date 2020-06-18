39846bbd8173773ad4109ca59be4cdf63f64b5c6
daoquang2k <66986060+daoquang2k@users.noreply.github.com>
2020-06-18 11:51:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="Connect.php" method="POST">
+	Username<input type="text" name="username" label="User" required="true"><br>
+	Password<input type="password" name="pass" lable="Pass" required="true"><br>
+	<input type="Submit" name="" id="" value="Login">
+</form>
+</body>
+</html>
